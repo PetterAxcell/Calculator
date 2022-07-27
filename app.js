94d21d3ef3230fe55e688f6caa4008f157c7c0ad
Petter Axcell <petteraxcell03@gmail.com>
2022-07-27 14:35:14 +0200

    Setup ready

---
+++ app.js
@@ -0,0 +1,13 @@
+let options = {
+    data: function(){
+        return {
+            
+        }
+    },
+    template: `
+    <span>  </span>
+    `,
+}
+
+var app = Vue.createApp(options);
+const vm = app.mount('#app');
