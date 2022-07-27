e3975889025f7f9c10edeba9676adc10af249a5a
Petter Axcell <petteraxcell03@gmail.com>
2022-07-27 14:53:42 +0200

    first version of a simplified calculator

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 let options = {
     data: function(){
         return {
-            
         }
     },
     template: `
-    <span>  </span>
+    <div>
+        <h1> CALCULATOR </h1>
+        <Screen> </Screen>
+        <NumbersCalculator></NumbersCalculator>
+        <SymbolsCalculator></SymbolsCalculator>
+    </div>
     `,
 }
 
 var app = Vue.createApp(options);
+
+app.component('NumbersCalculator',{
+    template:
+    `
+    <div v-for="row in 3">
+        <button v-for="number in 3">{{row*number}}</button>
+    </div>
+    `
+})
+app.component('Screen',{
+    template:
+    `
+    <input type="text">
+    `
+})
+app.component('SymbolsCalculator',{
+    template:
+    `
+    <button> + </button>
+    <button> - </button>
+    <button> x </button>
+    <button> / </button>
+
+    `
+})
 const vm = app.mount('#app');
